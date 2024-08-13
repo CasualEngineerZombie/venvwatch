7d810ed4c7a22ff3335ecca757d967977bccdced
CodePhilanthropist <barrigaete@gmail.com>
2024-08-14 02:20:17 +0800

    🔧 [3] chore: Start Github Actions for PyPi

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='venvwatch',
-    version='0.6',
-    description='Automatically updates requirements.txt by watching your virtual environment for changes',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Casual Engineer Zombie',
-    author_email='py@casualzom.dev',
+    name="venvwatch",
+    version="0.6",
+    description="Automatically updates requirements.txt by watching your virtual environment for changes",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Casual Engineer Zombie",
+    author_email="py@casualzom.dev",
     packages=find_packages(),
     install_requires=[
-        'watchdog',
+        "watchdog",
     ],
     entry_points={
-        'console_scripts': [
-            'venvwatch = venvwatch.cli:main',  # Changed from main to cli
+        "console_scripts": [
+            "venvwatch = venvwatch.cli:main",  # Changed from main to cli
         ],
     },
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-     project_urls={
-        'Source': 'https://github.com/CasualEngineerZombie/venvwatch',
-        'Tracker': 'https://github.com/CasualEngineerZombie/venvwatch/issues',
+    url="https://github.com/CasualEngineerZombie/venvwatch",
+    project_urls={
+        "Source": "https://github.com/CasualEngineerZombie/venvwatch",
+        "Tracker": "https://github.com/CasualEngineerZombie/venvwatch/issues",
+        "GitHub": "https://github.com/CasualEngineerZombie/venvwatch",
     },
 )
